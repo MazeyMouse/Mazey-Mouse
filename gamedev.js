5ee6dd17460cc91a53b9fa36c2c799d98c8ff13e
stefanoisdibo <210691@hkis.edu.hk>
2016-05-27 08:26:03 +0800

    added new code for mouse move

--- gamedev.js
+++ gamedev.js
@@ -1,14 +1,27 @@
-var $mouseX = 0, $mouseY = 0;
-var $xp = 0, $yp =0;
+<head>
+<style>
+#character {position:absolute;display:none;color:white;background-color:black}
+#onme {width:150px;height:80px;background-color:yellow;cursor:pointer}
+</style>
+<script type="text/javascript">
+var divName = 'character'; // div that is to follow the mouse (must be position:absolute)
+var offX = 15;          // X offset from mouse position
+var offY = 15;          // Y offset from mouse position
 
-$(document).mousemove(function(e){
-    $mouseX = e.pageX;
-    $mouseY = e.pageY;    
-});
+function mouseX(evt) {if (!evt) evt = window.event; if (evt.pageX) return evt.pageX; else if (evt.clientX)return evt.clientX + (document.documentElement.scrollLeft ?  document.documentElement.scrollLeft : document.body.scrollLeft); else return 0;}
+function mouseY(evt) {if (!evt) evt = window.event; if (evt.pageY) return evt.pageY; else if (evt.clientY)return evt.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop); else return 0;}
 
-var $loop = setInterval(function(){
-$xp += (($mouseX - $xp)/12);
-$yp += (($mouseY - $yp)/12);
-$("#character").css({left:$xp +'px', top:$yp +'px'});  
-}, 30);
+function follow(evt) {
+    var obj = document.getElementById(divName).style;
+    obj.left = (parseInt(mouseX(evt))+offX) + 'px';
+    obj.top = (parseInt(mouseY(evt))+offY) + 'px'; 
+    }
+document.onmousemove = follow;
+</script>
+</head>
+<body>
+<div id="character">test</div>
+<br><br>
+<div id='onme' onMouseover='document.getElementById(divName).style.display="block"' onMouseout='document.getElementById(divName).style.display="none"'>Mouse over this</div>
+</body>
 
