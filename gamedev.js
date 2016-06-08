3ee37523ffac13c2b1fce2fe26656039607e87c4
MazeyMouse <210691@hkis.edu.hk>
2016-06-08 21:03:03 +0800

    added new div for cat content

--- gamedev.js
+++ gamedev.js
@@ -4,3 +4,28 @@ $(document).bind('mousemove', function(e){
        top:   e.pageY
     });
 });
+
+
+    animateDiv();
+
+function makeNewPosition(){
+    
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() ;
+    var w = $(window).width() ;
+    
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
+}
+
+function animateDiv(){
+    var newq = makeNewPosition();
+  //James change .a to .othercar
+    $('.a').animate({ top: newq[0], left: newq[1] }, function(){
+      animateDiv();        
+    });
+    
+};
