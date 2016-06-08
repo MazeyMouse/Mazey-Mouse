4190b5be3d47c66db366c7ddb0ae822413cb7f8e
MazeyMouse <210691@hkis.edu.hk>
2016-06-08 09:07:23 +0800

    put off y

--- gamedev.js
+++ gamedev.js
@@ -1,7 +1,7 @@
 
 var divName = 'character'; // div that is to follow the mouse (must be position:absolute)
 var offX = 15;          // X offset from mouse position
-
+var offy = 15;
 
 function mouseX(evt) {if (!evt) evt = window.event; if (evt.pageX) return evt.pageX; else if (evt.clientX)return evt.clientX + (document.documentElement.scrollLeft ?  document.documentElement.scrollLeft : document.body.scrollLeft); else return 0;}
 
@@ -15,7 +15,7 @@ document.onmousemove = follow;
 
 
 
- 
+
     animateDiv();
 
 function makeNewPosition(){
