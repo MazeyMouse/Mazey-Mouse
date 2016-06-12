be0bb22c93c6fe6b8d4ffcab15f74304b744f9ba
MazeyMouse <210691@hkis.edu.hk>
2016-06-12 14:35:35 +0800

    added collision function

--- gamedev.js
+++ gamedev.js
@@ -32,5 +32,39 @@ function animateDiv(){
     
 };
 
-$("#gameover").hide
+$("#gameover").hide();
+function collision($div1, $div2) {
+    var x1 = $div1.offset().left;
+    var y1 = $div1.offset().top;
+    var h1 = $div1.outerHeight(true);
+    var w1 = $div1.outerWidth(true);
+    var b1 = y1 + h1;
+    var r1 = x1 + w1;
+    var x2 = $div2.offset().left;
+    var y2 = $div2.offset().top;
+    var h2 = $div2.outerHeight(true);
+    var w2 = $div2.outerWidth(true);
+    var b2 = y2 + h2;
+    var r2 = x2 + w2;
+    //below is an if statement - if the variables calculate to the right formula, it will return true or false
+    if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
+    return true;
+  }
+
+  window.setInterval(function() {
+    $('.a'), function() {
+      if (collision($('#character'), $(this))){
+
+                $("#gameover").show().delay(200).();
+
+        //all the actions that happen during a collision go here
+
+      }
+    });
+  }, 200); //this is how often it checks for a collision
+
+}); //document.ready ends - do not delete!!!
+      }
+
+
 
