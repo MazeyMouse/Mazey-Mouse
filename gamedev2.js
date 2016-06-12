c91a1d109bdde0403ab22e2ae25b185b60ede21e
MazeyMouse <210691@hkis.edu.hk>
2016-06-12 14:50:46 +0800

    added new explore.html

---
+++ gamedev2.js
@@ -0,0 +1,33 @@
+
+
+$(document).bind('mousemove', function(e){
+    $('#character').css({
+       left:  e.pageX + 20,
+       top:   e.pageY
+    });
+});
+
+
+    animateDiv();
+
+function makeNewPosition(){
+    
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() ;
+    var w = $(window).width() ;
+    
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
+}
+
+function animateDiv(){
+    var newq = makeNewPosition();
+  //James change .a to .othercar
+    $('.a').animate({ top: newq[0], left: newq[1] }, function(){
+      animateDiv();        
+    });
+    
+};
