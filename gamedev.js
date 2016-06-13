1d00190fac3da43f0b0ef4481ed3427dacb9445c
MazeyMouse <210691@hkis.edu.hk>
2016-06-13 21:26:37 +0800

    added collision function

--- gamedev.js
+++ gamedev.js
@@ -1,5 +1,11 @@
 
 
+$(document).ready(function() {
+
+   $("tryagain").hide();
+
+
+
 $(document).bind('mousemove', function(e){
     $('#character').css({
        left:  e.pageX + 20,
@@ -32,10 +38,38 @@ function animateDiv(){
     
 };
 
+function collision($div1, $div2) {
+    var x1 = $div1.offset().left;
+    var y1 = $div1.offset().top;
+    var h1 = $div1.outerHeight(true);
+    var w1 = $div1.outerWidth(true);
+    var b1 = y1 + h1;
+    var r1 = x1 + w1;
+    var x2 = $div2.offset().left;
+    var y2 = $div2.offset().top;
+    var h2 = $div2.outerHeight(true);
+    var w2 = $div2.outerWidth(true);
+    var b2 = y2 + h2;
+    var r2 = x2 + w2;
+    //below is an if statement - if the variables calculate to the right formula, it will return true or false
+    if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
+    return true;
+  }
 
-$(document).ready(function() {
+  window.setInterval(function() {
+    //function that makes the magic happen! Below, jQuery prints the word "FALSE" into #results
+  
+    //IMPORTANT!!! Below declares the class of divs that your sprite collides with!!
+    $.each($('.a'), function() {
+      if (collision($('#character'), $(this))) { //another if statement. If #myCar DOES hit something, the following will happen:
+        //if #myCar hits .othercar, then #results will say "TRUE"
+        $("#tryagain").show();
+        $("#character").hide();
+        $("#cat").hide();
 
-   $("tryagain").hide();
+
+
+
 
 
 
